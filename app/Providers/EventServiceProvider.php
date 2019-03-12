32ea4e55bc7e9b7fa67a5496556e730781249563
tanghao <tanghaolll@163.com>
2019-03-12 16:15:27 +0800

    完善支付逻辑

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-
+use App\Listeners\SendOrderPaidMail;
+use App\Events\OrderPaid;
+use App\Listeners\UpdateProductSoldCount;
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -18,6 +20,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        OrderPaid::class => [
+            UpdateProductSoldCount::class,
+            SendOrderPaidMail::class,
+        ],
+
     ];
 
     /**
